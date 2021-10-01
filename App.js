8cf63809c80153f5dd5a0f6f95b13a9fd42c00a5
XamuelUG <samweko@gmail.com>
2021-10-01 21:30:02 +0300

    creating Repo

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+//start
+import Onboard from './App/Start/Onboard';
+
+const App = () => {
+  return <Onboard />;
+};
+
+export default App;
---
+++ App/Start/Onboard.js
@@ -0,0 +1,102 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+
+//next
+import Login from './Login';
+
+const slides = [
+  {
+    key: 1,
+    title: 'TheBeacon App',
+    text: 'Welcome \nto TheBeacon App',
+    image: require('../../Resources/start/educ.png'),
+    backgroundColor: '#59b2ab',
+  },
+  {
+    key: 2,
+    title: 'Assignments',
+    text: 'Get Your \nAssignments \nAnd Notes in one place',
+    image: require('../../Resources/start/exams.png'),
+    backgroundColor: '#febe29',
+  },
+  {
+    key: 3,
+    title: 'Attendance',
+    text: 'Scan a QR code \nto Mark Your Attendance, \n\nIts Fun',
+    image: require('../../Resources/start/std_reading.png'),
+    backgroundColor: '#22bcb5',
+  },
+];
+
+class Onboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showRealApp: false,
+    };
+  }
+
+  _renderItem = ({item}) => {
+    return (
+      <View style={styles.slide}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Image source={item.image} style={styles.image} />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
+    );
+  };
+
+  _onDone = () => {
+    // User finished the introduction. Show real app through
+    // navigation or simply by controlling state
+    this.setState({showRealApp: true});
+  };
+
+  onSkip = () => {
+    this.setState({showRealApp: true});
+  };
+
+  render() {
+    if (this.state.showRealApp) {
+      return <Login />;
+    } else {
+      return (
+        <AppIntroSlider
+          renderItem={this._renderItem}
+          data={slides}
+          onSkip={this.onSkip}
+          showSkipButton={true}
+          onDone={this._onDone}
+        />
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  slide: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(58, 92, 170, 0.45)',
+  },
+  image: {
+    width: 250,
+    height: 250,
+    marginVertical: 48,
+    borderRadius: 250 / 2,
+    padding: 25,
+  },
+  text: {
+    // color: 'rgba(255, 255, 255, 0.8)',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 22,
+    // color: 'white',
+    textAlign: 'center',
+  },
+});
+
+export default Onboard;
